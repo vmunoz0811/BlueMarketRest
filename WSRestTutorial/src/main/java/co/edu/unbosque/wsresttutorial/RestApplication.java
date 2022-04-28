7dacf1cdc241a6784ad62e4f7c3d1129f6467fb6
mencisom@unbosque.edu.co <100002319+Mencisom@users.noreply.github.com>
2022-04-27 19:18:39 -0500

    logic2.0

--- WSRestTutorial/src/main/java/co/edu/unbosque/wsresttutorial/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unbosque.wsresttutorial;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class RestApplication extends Application {
-
-}
--- src/main/java/co/edu/unbosque/bluemarketrest/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unbosque.bluemarketrest;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
